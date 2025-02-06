7016a89e005fa0109a3dd2252129ea1c48f3628a
4mede <4mede@tutamail.com>
2025-02-06 11:40:10 -0500

    Revert "devon: define boot animation res"
    This reverts commit 56e2f14650bb5cd6a4ce7bf146d7bf093b7c482e.

--- pixelage_caprip.mk
+++ pixelage_caprip.mk
@@ -16,9 +16,6 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 # Inherit some common Pixelage stuff.
 $(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
 # Pixelage
 PIXELAGE_BUILDTYPE := UNOFFICIAL
 PIXELAGE_MAINTAINER := b
