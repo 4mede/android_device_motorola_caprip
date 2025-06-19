b5f58749d1d0bf69fc098c4f3888e23272df6285
Debarpan102 <debarpanhalder8@gmail.com>
2025-06-19 17:30:00 +0530

    borneo: Remove Firmware Extraction

--- extract-files.py
+++ extract-files.py
@@ -5,7 +5,6 @@
 #
 
 from extract_utils.extract import extract_fns_user_type
-from extract_utils.extract_star import extract_star_firmware
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -40,10 +39,6 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
 }  # fmt: skip
 
-extract_fns: extract_fns_user_type = {
-    r'(bootloader|radio)\.img': extract_star_firmware,
-}
-
 module = ExtractUtilsModule(
     'caprip',
     'motorola',
@@ -51,7 +46,6 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     extract_fns=extract_fns,
-    add_firmware_proprietary_file=True,
     add_generated_carriersettings=True,
 )
 
